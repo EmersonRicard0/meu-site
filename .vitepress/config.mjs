0791b36cd6531746fcacb72681acecc5f2c9db35
Emerson Silva Ricardo <92190123+EmersonRicard0@users.noreply.github.com>
2025-09-01 15:00:36 -0300

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,27 +1,5 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  // Título do site (aparece no canto superior esquerdo)
-  title: 'Emerson Silva Ricardo',
-  description: 'Analista de Redes / NOC - Portfólio Profissional',
-
-  themeConfig: {
-    // Itens do menu de navegação
-    nav: [
-      { text: 'Início', link: '/' },
-      { text: 'Currículo', link: '/curriculo' },
-      { text: 'Contato', link: '/contato' }
-    ], // <-- Vírgula importante aqui
-
-    // Ícones de redes sociais
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/EmersonRicard0' },
-      { icon: 'linkedin', link: 'https://www.linkedin.com/in/emerson-silva-ricardo-543308119/' }
-    ]
-  },
-  
-  // Ícone da aba do navegador
-  head: [
-    ['link', { rel: 'icon', href: '/img/logo.png' }]
-  ]
+  title: 'TESTE DO SITE'
 })
